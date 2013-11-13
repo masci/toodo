45efb042c615098cab007476ad21fd1611293cb2
masci <mpippi@gmail.com>
2013-11-13 18:31:00 +0100

    static root dir

--- toodo/toodo/settings.py
+++ toodo/toodo/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'core',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -101,6 +103,5 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.6/howto/static-files/
-
+STATIC_ROOT = 'static'
 STATIC_URL = '/static/'
