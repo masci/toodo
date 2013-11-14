41949f3c9f048e1080c341bf80bdf023613b3526
masci <mpippi@gmail.com>
2013-11-14 09:43:44 +0100

    fixed BASE_PATH

--- toodo/toodo/settings.py
+++ toodo/toodo/settings.py
@@ -1,20 +1,12 @@
 """
 Django settings for toodo project.
 
-For more information on this file, see
-https://docs.djangoproject.com/en/1.6/topics/settings/
-
-For the full list of settings and their values, see
-https://docs.djangoproject.com/en/1.6/ref/settings/
 """
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '#%p4s@1(+!0&)^=sl91!j^#x_^98)+(acf)5#pblu$u4s2g+_='
@@ -38,6 +30,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
 
     'core',
+    'rest_framework',
 )
 
 MIDDLEWARE_CLASSES = (
