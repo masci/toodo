a44e5b7b041f4822ca678f8aaf8f25a5608e98c5
masci <mpippi@gmail.com>
2013-11-14 17:38:50 +0100

    added DOT urls

--- toodo/toodo/urls.py
+++ toodo/toodo/urls.py
@@ -5,6 +5,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^api/', include('core.urls')),
+    url(r'^o/', include('oauth2_provider.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
 )
